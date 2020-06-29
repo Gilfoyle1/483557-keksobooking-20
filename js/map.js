35af8cbc4c9a024eb0c54111274ce8441b1b2e13
Gilfoyle1 <kostomarov.e@gmail.com>
2020-06-29 19:38:08 +0300

    Личный проект: модуляция (часть 2)

--- js/map.js
+++ js/map.js
@@ -12,8 +12,11 @@
   };
 
   var Pin = {
-    WIDTH: 50,
-    HEIGHT: 70
+    WIDTH: 65,
+    HEIGHT: 82,
+    MIN_Y: 130,
+    MAX_Y: 630,
+    MIN_X: 1
   };
 
   var formElements = document.querySelectorAll('.map__filter, fieldset');
@@ -32,24 +35,80 @@
   };
 
   var getPinCoordinates = function () {
-    var x = map.classList.contains('map--faded') ? PinMain.X + PinMain.WIDTH / 2 : PinMain.X + Pin.WIDTH / 2;
-    var y = map.classList.contains('map--faded') ? PinMain.Y + PinMain.HEIGHT / 2 : PinMain.Y + Pin.HEIGHT;
+    var coordinateX = parseInt(mapPinMain.style.left, 10);
+    var coordinateY = parseInt(mapPinMain.style.top, 10);
+
+    var x = map.classList.contains('map--faded') ? PinMain.X + Math.floor(PinMain.WIDTH / 2) : coordinateX + Math.floor(Pin.WIDTH / 2);
+    var y = map.classList.contains('map--faded') ? PinMain.Y + Math.floor(PinMain.HEIGHT / 2) : coordinateY + Pin.HEIGHT;
 
     return x + ', ' + y;
   };
 
   var onPinClick = function (evt) {
     if (evt.which === LEFT_BUTTON_MOUSE) {
-      activateMap();
-    }
-  };
+      if (map.classList.contains('map--faded')) {
+        activateMap();
+      }
 
-  var getAddressValue = function () {
-    adFormAddress.value = getPinCoordinates();
+      var referencePoint = {
+        x: evt.clientX,
+        y: evt.clientY
+      };
+
+      var onMouseMove = function (moveEvt) {
+        moveEvt.preventDefault();
+
+        var minСoordinatesX = Pin.MIN_X - PinMain.WIDTH / 2;
+        var maxСoordinatesX = map.offsetWidth - PinMain.WIDTH / 2;
+        var minСoordinatesY = Pin.MIN_Y - Pin.HEIGHT;
+        var maxСoordinatesY = Pin.MAX_Y - Pin.HEIGHT;
+
+        var glidePin = {
+          x: referencePoint.x - moveEvt.clientX,
+          y: referencePoint.y - moveEvt.clientY
+        };
+
+        referencePoint = {
+          x: moveEvt.clientX,
+          y: moveEvt.clientY
+        };
+
+        mapPinMain.style.top = (mapPinMain.offsetTop - glidePin.y) + 'px';
+        mapPinMain.style.left = (mapPinMain.offsetLeft - glidePin.x) + 'px';
+
+        if (mapPinMain.offsetLeft < minСoordinatesX) {
+          mapPinMain.style.left = minСoordinatesX + 'px';
+        } else if (mapPinMain.offsetLeft > maxСoordinatesX) {
+          mapPinMain.style.left = maxСoordinatesX + 'px';
+        }
+
+        if (mapPinMain.offsetTop < minСoordinatesY) {
+          mapPinMain.style.top = minСoordinatesY + 'px';
+        } else if (mapPinMain.offsetTop > maxСoordinatesY) {
+          mapPinMain.style.top = maxСoordinatesY + 'px';
+        }
+
+        getAddressValue();
+      };
+
+      var onMouseUp = function (upEvt) {
+        upEvt.preventDefault();
+
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+      };
+
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+    }
   };
 
   var onPinEnterPress = function (evt) {
     window.utils.isEnterEvent(evt, activateMap);
+  };
+
+  var getAddressValue = function () {
+    adFormAddress.value = getPinCoordinates();
   };
 
   var onAdOpen = function (ad) {
@@ -92,7 +151,6 @@
     window.pin.render(COUNT);
     window.form.addValidation();
 
-    mapPinMain.removeEventListener('mousedown', onPinClick);
     mapPinMain.removeEventListener('keyup', onPinEnterPress);
   };
 
