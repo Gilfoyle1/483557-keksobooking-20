12805b9a40ace63d1d2788d4b7f7f0fef7b709bb
Gilfoyle1 <kostomarov.e@gmail.com>
2020-07-16 22:41:26 +0300

    еще сокращения убраны

--- js/pin.js
+++ js/pin.js
@@ -12,22 +12,22 @@
   var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
   var generatePin = function (pin) {
-    var adElement = pinTemplate.cloneNode(true);
+    var advertElementnt = pinTemplate.cloneNode(true);
 
-    adElement.style.left = pin.location.x - Pin.WIDTH / 2 + 'px';
-    adElement.style.top = pin.location.y - Pin.HEIGHT + 'px';
-    adElement.querySelector('img').src = pin.author.avatar;
-    adElement.querySelector('img').alt = pin.offer.title;
+    advertElementnt.style.left = pin.location.x - Pin.WIDTH / 2 + 'px';
+    advertElementnt.style.top = pin.location.y - Pin.HEIGHT + 'px';
+    advertElementnt.querySelector('img').src = pin.author.avatar;
+    advertElementnt.querySelector('img').alt = pin.offer.title;
 
-    adElement.addEventListener('click', function () {
+    advertElementnt.addEventListener('click', function () {
       window.map.onAdOpen(pin);
-      adElement.classList.add('map__pin--active');
+      advertElementnt.classList.add('map__pin--active');
     });
-    adElement.addEventListener('keyup', function (evt) {
+    advertElementnt.addEventListener('keyup', function (evt) {
       window.utils.isEnterEvent(evt, window.map.onAdOpen, pin);
     });
 
-    return adElement;
+    return advertElementnt;
   };
 
   var renderAllPins = function (pinsAmount) {
